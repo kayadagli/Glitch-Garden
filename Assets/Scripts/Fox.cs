c728d1bccbbc182b9a7a6c997045255f3e7b0722
Kaya Dağlı <kayadagli27@gmail.com>
2021-04-05 16:09:17 +0300

    Glitch Garden

---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        GameObject otherObject = otherCollider.gameObject;
+
+        // If the other object has a Defender component,
+        // call our public attack method.
+
+        //if this is a gravestone then jump.
+        if (otherObject.GetComponent<Gravestone>())
+        {
+            GetComponent<Animator>().SetTrigger("jumpTrigger");
+        }
+
+        else if (otherObject.GetComponent<Defender>())
+        {
+            GetComponent<Attacker>().Attack(otherObject);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Lizard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lizard : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        GameObject otherObject = otherCollider.gameObject;
+        
+        // If the other object has a Defender component,
+        // call our public attack method.
+
+        if (otherObject.GetComponent<Defender>())
+        {
+            GetComponent<Attacker>().Attack(otherObject);
+        }
+
+    }
+
+
+}
